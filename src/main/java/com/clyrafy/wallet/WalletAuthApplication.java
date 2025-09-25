262d5ca74ace4b2b3ba454d447654d599932d99d
officialsanchi <nwangwudinma22@gmail.com>
2025-09-25 21:49:45 +0100

    updated

--- src/main/java/com/clyrafy/wallet/WalletAuthApplication.java
+++ src/main/java/com/clyrafy/wallet/WalletAuthApplication.java
@@ -3,7 +3,10 @@ package com.clyrafy.wallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableAsync
@@ -11,5 +14,17 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class WalletAuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(WalletAuthApplication.class, args);
+
     }
-}
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+
+        }
