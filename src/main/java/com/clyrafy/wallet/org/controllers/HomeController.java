7d4f72e3bbc96490b3c95344500d887e46569c13
officialsanchi <nwangwudinma22@gmail.com>
2025-09-26 12:46:02 +0100

    working deployed app

--- src/main/java/com/clyrafy/wallet/org/controllers/HomeController.java
+++ src/main/java/com/clyrafy/wallet/org/controllers/HomeController.java
@@ -1,14 +1,14 @@
-package com.clyrafy.wallet.org.controllers;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HomeController {
-
-    @GetMapping("/")
-    public String ourHomeController() {
-        return "Server is working";
-    }
-}
+//package com.clyrafy.wallet.org.controllers;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class HomeController {
+//
+//    @GetMapping("/")
+//    public String ourHomeController() {
+//        return "Server is working";
+//    }
+//}
